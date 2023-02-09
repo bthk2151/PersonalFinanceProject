c60681b0549d9100dc9ae2acc2670edd13871ec2
Bryan Tan Hoe Kin <bthk2151@gmail.com>
2023-02-09 21:45:58 +0800

    Renamed main directory as webapp instead of personal_finance_project

--- webapp/api/views.py
+++ webapp/api/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework import generics
 from .serializers import UserSerializer
 from .models import User
 
 
-class UserView(generics.CreateAPIView):
+class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
