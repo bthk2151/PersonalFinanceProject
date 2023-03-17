0f9c2dba06b09aa1c1e5b44651a08647936bb6a0
Bryan Tan Hoe Kin <bthk2151@gmail.com>
2023-03-17 22:49:15 +0800

    Swapped Roboto CDN to use local npm package instead, removed and gitignored all webpack generated files

--- webapp/frontend/src/index.js
+++ webapp/frontend/src/index.js
@@ -7,6 +7,11 @@ import { BrowserRouter } from "react-router-dom";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 const app = $("#app")[0];
 const root = createRoot(app);
 
