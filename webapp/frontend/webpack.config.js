77c3d040f9a1102576f4968379a7f011ce9e6912
Bryan Tan Hoe Kin <bthk2151@gmail.com>
2023-03-17 23:17:24 +0800

    Updated webpack config to ensure similar format regex matching for file types to bundle

--- webapp/frontend/webpack.config.js
+++ webapp/frontend/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.js$/,
+        test: /\.js$/i,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
